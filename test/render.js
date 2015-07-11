0cc53390ec93fe4c372657b58a8fc03074f6fcd9
Yaron Naveh <yaronn01@gmail.com>
2015-07-11 14:53:44 +0000

    parse arrays and types

--- test/render.js
+++ test/render.js
@@ -1,6 +1,6 @@
 
 var fs = require('fs')
-  , xml = fs.readFileSync('./doc.xml')
+  , xml = fs.readFileSync('./rich.xml')
   , parse = require('xml2js').parseString
   , Viewer = require('../lib/document-viewer.js')
 
@@ -8,8 +8,8 @@ parse(xml, function (err, doc) {
   var viewer = new Viewer(doc.document)
   viewer.renderPage(0)
 
-  setTimeout(function() {viewer.renderPage(1)}, 1000)
-  setTimeout(function() {viewer.renderPage(0)}, 2000)
+  //setTimeout(function() {viewer.renderPage(1)}, 1000)
+  //setTimeout(function() {viewer.renderPage(0)}, 0)
   
 
 });
